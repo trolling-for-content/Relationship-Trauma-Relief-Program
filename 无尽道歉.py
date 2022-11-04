60ab5b84c4dc49dc3da5934dfe079717dfecb168
2439905184 <suchenglong018@126.com>
2022-11-04 19:54:57 +0800

    🎉 init: init

---
+++ 无尽道歉.py
@@ -0,0 +1,5 @@
+ch = input("请输入你要发泄的恋爱对象名字\n")
+sentence = input("请输入你要发泄的话\n")
+while True:
+  print(ch + ": 对不起，我错了")
+  
---
+++ 逐行道歉.py
@@ -0,0 +1,4 @@
+ch = input("请输入你要发泄的恋爱对象名字\n")
+while True:
+    sentence = input("请输入你要发泄的话\n")
+    print(ch + ": 对不起，我错了")
